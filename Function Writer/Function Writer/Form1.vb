7855b95e67dfb4893bad4f436aca15886c3b0eda
365Proz (by Paresh Sharma) <63383145+365Proz@users.noreply.github.com>
2020-04-13 14:08:38 +0000

    14/04/2020

---
+++ Function Writer/Function Writer/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Function fnWordCounter(ByVal Text As String) As Integer
+        Dim letter As Char
+        ''Dim tempString As String = ""
+        Dim wordCount As Integer = 0
+        Dim i As Integer
+
+        If Text.Length > 0 Then
+            For i = 0 To Text.Length - 1
+                letter = Text.Chars(i)
+                If letter = " " Then
+                    wordCount += 1
+                End If
+            Next
+            Return wordCount + 1
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(fnWordCounter(inputTextBox.Text))
+    End Sub
+End Class
---
+++ Function Writer/Function Writer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Function Writer")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Function Writer")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ea0c55d5-b565-406d-b202-5a3333cb798c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
